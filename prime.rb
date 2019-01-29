670b3eade92ed314306fa9a7f89fc38481f1dc19
active-object-6703 <nerajno@gmail.com>
2019-01-29 15:08:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 # Add  code here!
 def prime?(number)
+  #if number is / by itself and one return is true
+  # else spout false
+  if number/ 1 % 0  & number / number % 0
+    return true
+  else
+    return false
+  end
 
 end
-    
