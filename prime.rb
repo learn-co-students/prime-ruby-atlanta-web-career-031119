16e1303c0ec6541ddbe06ff447eb03bafb333d5a
active-object-6703 <nerajno@gmail.com>
2019-01-30 02:22:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,11 @@ def prime?(number)
   #if number is / by itself and one return is true
   # else spout false
   if number % 1 == 0 && number % number == 0
-return true
-elsif number % 1 != 0 && number % number != 0
+    return true
+  elsif number % 1 != 0 && number % number != 0
     return false
-else number == -1
-  return false
+  else number == -1
+   return false
   end
 
 end
