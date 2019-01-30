3c867f0b7e35411e33e9398624bcd93e0a97299d
active-object-6703 <nerajno@gmail.com>
2019-01-30 02:38:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,28 @@
 
 
 # Add  code here!
+# def prime?(number)
+#   #if number is / by itself and one return is true
+#   # else spout false
+#   if number % 1 == 0 && number % number == 0
+#     return true
+#   elsif number % 1 != 0 && number % number != 0
+#     return false
+#   else number  1
+#    return false
+#   end
+#
+# end
+require 'benchmark'
+
 def prime?(number)
   #if number is / by itself and one return is true
   # else spout false
-  if number % 1 == 0 && number % number == 0
-return true
-
-elsif number % 1 != 0 && number % number != 0
+  if number <=1
+    return false
+  elsif number % 1 == 0 && number % number == 0
+    return true
+  elsif number % 1 != 0 && number % number != 0
     return false
   end
 
