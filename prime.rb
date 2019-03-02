f9ba73bf0ed458270b31d382014e147baffe7ce2
Travis Martin <travisemerymartin@gmail.com>
2019-03-02 22:56:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@
 def prime?(number)
   if number <= 1
     return false
-  elsif number != 2 && number != 3
+  elsif number == 2 || number == 3
+    return true
+  elsif number%2 != 0 || number%3 != 0
     i=5
     while i*i <= number
       if number%i == 0 || number%(i+2) == 0
