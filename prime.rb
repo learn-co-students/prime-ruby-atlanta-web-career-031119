ff6e5554fdba44b159f007da67bb92f589e34206
Travis Martin <travisemerymartin@gmail.com>
2019-03-02 22:53:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,15 +1,14 @@
 # Add  code here!
 def prime?(number)
-  if number > 3
-    if number%2 == 0 || number%3 ==0
-      return false
-    end
+  if number <= 1
+    return false
+  elsif number != 2 && number != 3
     i=5
-    while i < number
+    while i*i <= number
       if number%i == 0 || number%(i+2) == 0
         return false
       end
-      i += 1
+      i += 6
     end
   end
   return true
