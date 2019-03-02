f0a4fafc7bb9f9aa3206b57a217e1f5499d31b3b
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 23:26:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  for divide in 2..100
+    if number % divide == 0 and number != divide
+      return false
+    end
+  end
+  return true
+end
