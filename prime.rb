9c6805bc95949bc261fa9cf9150436f27c4c2cb4
Preston Elliott <preston.elliott24@gmail.com>
2019-03-06 18:10:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
+end
+
+# Previous work
+# def prime?(num)
+#   (2..(num - 1)).each do |n|
+#     if num % n == 0
+#       return true
+#     else
+#       false 
+#     end
+#   end
+# end
+
+# def prime?(num)
+#   (2..(num - 1)).each do |n|
+#     return false if num % n == 0
+#   end
+#   true
+# end
+
